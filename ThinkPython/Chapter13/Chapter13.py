1eb0ae2936e28ca88d3f50b8950eb809de82ed34
peter.mulholland <peter.mulholland@wonga.com>
2014-04-08 13:37:14 +0100

    Chanpter 13 exercise & common library

--- ThinkPython/Chapter13/Chapter13.py
+++ ThinkPython/Chapter13/Chapter13.py
@@ -1,8 +1,47 @@
 import string
+from TPCommon import *
 
+def count_total_words(words):
+    total = 0
+    for word, count in words.items():
+        total += count
+    
+    print total, "words used"
+
+
+
+def count_different_words(words):
+    print len(words), "different words used"
+    
+def print_most_frequent(words, count):
+    freq = zip(words.values(), words.keys())
+    freq.sort(reverse=True)
+
+    counter = 0;
+    for num, word in freq:
+        print word, "occurs", num, "times"
+        counter += 1
+        if counter >= count:
+            break
+
+def analyse_book(file):
+    words = read_gutenberg_book(file)
+    count_total_words(words)
+    count_different_words(words)
+    # ex 13.3
+    print_most_frequent(words, 20)
+
+def ex_13_2():
+    # todo: downlaod several books, create a list of names and run them through
+    analyse_book("LovecraftShunnedHouse.txt")
 
 if __name__ == '__main__':
-    print string.whitespace
-    print string.punctuation
+    #print string.whitespace
+    #print string.punctuation
 
+    #words = read_words_file()
+    #print len(words), " word read"
+    #words = read_words_from_file("TestRead.txt")
+    #print words
+    ex_13_2()
 
---
+++ ThinkPython/TPCommon/TPCommon.py
@@ -0,0 +1,62 @@
+import string
+
+def get_words_from_line(line, words):
+    line = line.replace('.', ' ')
+    line = line.replace('_', ' ')
+    line = line.replace('-', ' ')
+    line = line.replace('://', ' ')
+    line_words = line.split()
+    for word in line_words:
+        word = word.translate(None, string.punctuation)
+        word = word.strip(string.punctuation)
+        if word.isalpha():
+            word = word.lower()
+            words[word] = words.get(word, 0) + 1
+
+def read_gutenberg_book(file):
+    fin = open(file)
+    words = dict()
+    header_read = False
+
+    for line in fin:
+        if not header_read:
+            if  "START OF THIS PROJECT GUTENBERG EBOOK" in line:
+                header_read = True
+
+            continue
+
+        if "END OF THIS PROJECT GUTENBERG EBOOK" in line:
+            break
+
+        get_words_from_line(line, words)
+
+    return words
+
+def read_words_from_file(file):
+    fin = open(file)
+    words = dict()
+    for line in fin:
+        get_words_from_line(line, words)
+
+    return words
+
+
+def read_words_file():
+    fin = open('..\words.txt') 
+    words = []
+    for line in fin: 
+        word = line.strip()
+        words.append(word)
+
+    return words
+
+def make_histogram(s):
+    """Make a map from letters to number of times they appear in s.
+    s: string
+    Returns: map from letter to frequency
+    """
+    hist = {}
+    for x in s:
+        hist[x] = hist.get(x, 0) + 1
+    return hist
+
