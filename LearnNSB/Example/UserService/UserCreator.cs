5a33ec758c08ff3190bde8501878d8fb00506903
Peter Mulholland <peter.mulholland@yahoo.co.uk>
2014-04-11 18:33:17 +0100

    LearnNSB start

---
+++ LearnNSB/Example/UserService/UserCreator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+using UserService.Messages.Commands;
+
+namespace UserService
+{
+    public class UserCreator : IHandleMessages<CreateNewUserCmd>
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof (UserCreator));
+
+        public void Handle(CreateNewUserCmd message)
+        {
+            log.InfoFormat("Creating user '{0}' with email '{1}'", message.Name, message.EmailAddress);
+        }
+    }
+}
