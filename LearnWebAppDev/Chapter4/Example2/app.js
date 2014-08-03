931fabf85c2fd11e5f59455245635cb2db1dd783
Peter Mulholland <peter.mulholland@yahoo.co.uk>
2014-08-03 18:49:56 +0100

    WebAppDev ch4 ex2 starting interactivity

--- LearnWebAppDev/Chapter4/Example2/app.js
+++ LearnWebAppDev/Chapter4/Example2/app.js
@@ -1,7 +1,8 @@
 var main = function () {
     "use strict";
-    
-    //window.alert("Hello World");
-}
+	$(".comment-input button").on("click", function(event) {
+		$(".comments").append("<p>this is a new comment</p>");
+	});
+};
 
 $(document).ready(main);
