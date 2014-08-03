dfb6bfd3d71997eaa8bcdd5cdacb7974bb0bc824
Peter Mulholland <peter.mulholland@yahoo.co.uk>
2014-08-03 20:14:33 +0100

    WebAppDev ch4 ex2, functionality

--- LearnWebAppDev/Chapter4/Example2/app.js
+++ LearnWebAppDev/Chapter4/Example2/app.js
@@ -1,11 +1,26 @@
 var main = function () {
     "use strict";
-	$(".comment-input button").on("click", function(event) {
-		var $new_comment = $("<p>"),
-			comment_text = $(".comment-input input").val();
+	
+	var addCommentFromInputBox = function() {
+		var $new_comment;
 		
-		$new_comment.text(comment_text)
-		$(".comments").append($new_comment);
+		if ($(".comment-input input").val() !== "") {
+			$new_comment = $("<p>").text($(".comment-input input").val());
+			$new_comment.hide();
+			$(".comments").append($new_comment);
+			$new_comment.fadeIn();
+			$(".comment-input input").val("");
+		}
+	}
+
+	$(".comment-input button").on("click", function(event) {
+		addCommentFromInputBox();
+	});
+	
+	$(".comment-input input").on("keypress", function (event) {
+		if (event.keyCode === 13) {
+			addCommentFromInputBox();
+		}
 	});
 };
 
