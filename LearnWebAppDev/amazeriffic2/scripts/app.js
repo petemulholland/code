29372c138ec3deae09073e8eee00ce20dc886763
peter.mulholland <peter.mulholland@wonga.com>
2014-08-06 17:15:13 +0100

    WebAppDev updating click handling

--- LearnWebAppDev/amazeriffic2/scripts/app.js
+++ LearnWebAppDev/amazeriffic2/scripts/app.js
@@ -8,6 +8,16 @@ var main = function() {
             $(element).addClass("active");
             $("main .content").empty();
             
+            // figure out which tab has been clicked
+            if ($(element).parent().is(":nth-child(1)")) {
+                console.log("FIRST TAB CLICKED!");
+            } else if ($(element).parent().is(":nth-child(2)")) {
+                console.log("SECOND TAB CLICKED!");
+            } else if ($(element).parent().is(":nth-child(3)")) {
+                console.log("THIRD TAB CLICKED!");
+            }
+            
+            
             return false;
         });
     });
