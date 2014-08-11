64f5276e719d1f3b8b04b9fdfca1172ff8774f17
peter.mulholland <peter.mulholland@wonga.com>
2014-08-11 14:04:14 +0100

    WebAppDev ch5 flickr & amaze copy

---
+++ LearnWebAppDev/Chapter5/amazeriffic/scripts/app.js
@@ -0,0 +1,101 @@
+var toDos = [
+    "finish chapter 4",
+    "take indie to the beach", 
+    "answer emails", 
+    "Prep for mondays class", 
+    "Make up some new Todos", 
+    "Get Groceries"
+];
+    
+var onOldest = function () {
+    var $content;
+
+    $content = $("<ul>");
+    toDos.forEach(function (todo) {
+        $content.append($("<li>").text(todo));
+    });
+    
+    $("main .content").append($content);
+}
+
+var onNewest = function () {
+    var $content;
+
+    var $content;
+
+    $content = $("<ul>");
+    toDos.forEach(function (todo) {
+        $content.prepend($("<li>").text(todo));
+    });
+    
+    $("main .content").append($content);
+}
+
+var addTodoFromInputBox = function() {
+    var $new_comment;
+    
+    if ($("main .content input").val() !== "") {
+        toDos.push($("main .content input").val());
+        $("main .content input").val("");
+    }
+}
+
+var onAdd = function () {
+    var $content;
+
+    //<input type="text"><button>+</button>
+    $content = $("<p>"); // cause p & ul have 30px margin applied in css.
+    
+    $content.append($('<input type="text">'));
+    $content.append($('<button>+</button>'));
+    
+    $("main .content").append($content);
+
+    // add the functionality for the controls.
+    $("main .content button").on("click", function(event) {
+        addTodoFromInputBox();
+    });
+
+    $("main .content input").on("keypress", function (event) {
+        if (event.keyCode === 13) {
+            addTodoFromInputBox();
+        }
+    });
+
+}
+
+var onClick = function (element) {
+   // create click handler for the element:
+    $(element).on("click", function() {
+        var $element = $(element);
+            
+        $(".tabs span").removeClass("active");
+        $element.addClass("active");
+        $("main .content").empty();
+        
+        // figure out which tab has been clicked
+        if ($element.parent().is(":nth-child(1)")) {
+            onNewest();
+        } else if ($element.parent().is(":nth-child(2)")) {
+            onOldest();
+        } else if ($element.parent().is(":nth-child(3)")) {
+            onAdd();
+        }
+        
+        return false;
+    });
+}
+
+var main = function() {
+    "use strict";
+    
+    $(".tabs span").toArray().forEach(function (element) {
+        onClick(element);
+    });
+    
+    // trigger fake click on first tab to load dynamic data:
+    $(".tabs a:first-child span").trigger("click");
+    
+};
+
+$(document).ready(main);
