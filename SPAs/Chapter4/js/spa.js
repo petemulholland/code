561e10d6bbeda5fc69f109e797706e18c59f1eac
peter.mulholland <peter.mulholland@wonga.com>
2014-08-20 12:12:32 +0100

    SPAs starting ch4

---
+++ SPAs/Chapter4/js/spa.js
@@ -0,0 +1,91 @@
+/*
+ * spa.js
+ * Root namespace module
+ *
+*/
+
+/*jslint         browser : true, continue : true,
+  devel  : true, indent  : 2,    maxerr   : 50,
+  newcap : true, nomen   : true, plusplus : true,
+  regexp : true, sloppy  : true, vars     : false,
+  white  : true
+*/
+
+/*global $, spa */
+
+var spa = (function () {
+
+  //---------------- BEGIN MODULE SCOPE VARIABLES --------------
+  /*var
+    configMap = {
+      settable_map : { color_name: true },
+      color_name   : 'blue'
+    },
+    stateMap  = { $container : null },
+    jqueryMap = {},
+
+    setJqueryMap, configModule, initModule;*/
+  //----------------- END MODULE SCOPE VARIABLES ---------------
+
+  //------------------- BEGIN UTILITY METHODS ------------------
+  // example : getTrimmedString
+  //-------------------- END UTILITY METHODS -------------------
+
+  //--------------------- BEGIN DOM METHODS --------------------
+  // Begin DOM method /setJqueryMap/
+  /*setJqueryMap = function () {
+    var $container = stateMap.$container;
+
+    jqueryMap = { $container : $container };
+  };*/
+  // End DOM method /setJqueryMap/
+  //---------------------- END DOM METHODS ---------------------
+
+  //------------------- BEGIN EVENT HANDLERS -------------------
+  // example: onClickButton = ...
+  //-------------------- END EVENT HANDLERS --------------------
+
+
+
+  //------------------- BEGIN PUBLIC METHODS -------------------
+  // Begin public method /configModule/
+  // Purpose    : Adjust configuration of allowed keys
+  // Arguments  : A map of settable keys and values
+  //   * color_name - color to use
+  // Settings   :
+  //   * configMap.settable_map declares allowed keys
+  // Returns    : true
+  // Throws     : none
+  //
+  /*configModule = function ( input_map ) {
+    spa.butil.setConfigMap({
+      input_map    : input_map,
+      settable_map : configMap.settable_map,
+      config_map   : configMap
+    });
+    return true;
+  };*/
+  // End public method /configModule/
+
+  // Begin public method /initModule/
+  // Purpose    : Initializes module
+  // Arguments  :
+  //  * $container the jquery element used by this feature
+  // Returns    : true
+  // Throws     : none
+  //
+  var initModule = function ( $container ) {
+    /*stateMap.$container = $container;
+    setJqueryMap();
+    return true;*/
+    spa.shell.initModule( $container );
+  };
+  // End public method /initModule/
+
+  // return public methods
+  return {
+    //configModule : configModule,
+    initModule   : initModule
+  };
+  //------------------- END PUBLIC METHODS ---------------------
+}());
