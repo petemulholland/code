bc1f122684110a4a5a94a42aa6f76a6b33e8b428
peter.mulholland <peter.mulholland@wonga.com>
2014-08-22 15:06:16 +0100

    SPAs starting chapter 5

---
+++ SPAs/Chapter5/js/spa.js
@@ -0,0 +1,35 @@
+/*
+ * spa.js
+ * Root namespace module
+ *
+*/
+
+/*jslint         browser : true, continue : true,
+  devel  : true, indent  : 2,    maxerr   : 50,
+  newcap : true, nomen   : true, plusplus : true,
+  regexp : true, sloppy  : true, vars     : false,
+  white  : true
+*/
+
+/*global $, spa */
+
+var spa = (function () {
+
+  // Begin public method /initModule/
+  // Purpose    : Initializes module
+  // Arguments  :
+  //  * $container the jquery element used by this feature
+  // Returns    : true
+  // Throws     : none
+  //
+  var initModule = function ( $container ) {
+    spa.shell.initModule( $container );
+  };
+  // End public method /initModule/
+
+  // return public methods
+  return {
+    initModule   : initModule
+  };
+  //------------------- END PUBLIC METHODS ---------------------
+}());
