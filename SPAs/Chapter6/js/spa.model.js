21a5e41aa9d52eff662ddebf63fed6e271829e90
peter.mulholland <peter.mulholland@wonga.com>
2014-08-29 18:06:03 +0100

    SPAs 6.3.4 fixing bugs in book, TODO: appendix B on TDD

--- SPAs/Chapter6/js/spa.model.js
+++ SPAs/Chapter6/js/spa.model.js
@@ -212,14 +212,13 @@ spa.model = (function () {
     //   * logout()- revert the current user object to anonymous.
     //     This method publishes a 'spa-logout' global custom event.
     logout = function () {
-      var is_removed, user = stateMap.user;
+      var user = stateMap.user;
 
       chat._leave();
-      is_removed = removePerson( user );
       stateMap.user = stateMap.anon_user;
+      clearPeopleDb();
 
       $.gevent.publish( 'spa-logout', [ user ] );
-      return is_removed;
     };
     // End people method /logout/
 
@@ -292,7 +291,7 @@ spa.model = (function () {
 
     //------------------- BEGIN INTERNAL METHODS ----------------
     _update_list = function( arg_list ){
-      var i, person_map, make_person_map,
+      var i, person_map, make_person_map, person, 
         people_list = arg_list[ 0 ],
         is_chatee_online = false;
 
@@ -317,11 +316,12 @@ spa.model = (function () {
             id      : person_map._id,
             name    : person_map.name
           };
+          person = makePerson( make_person_map );
 
           if ( chatee && chatee.id === make_person_map.id ) {
             is_chatee_online = true;
+            chatee = person;
           }
-          makePerson( make_person_map );
         }
 
         stateMap.people_db.sort( 'name' );
