93da509bb1fe58e792e3ff84b06da58f9c966b98
peter.mulholland <peter.mulholland@wonga.com>
2014-11-18 11:11:16 +0000

    Python TDD ch5 functional test pass with cheat

--- Python/TDD/superlists/functional_tests.py
+++ Python/TDD/superlists/functional_tests.py
@@ -27,19 +27,23 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(inputbox.get_attribute('placeholder'),
                          'Enter a to-do item')
         
+        new_todo_text = 'Buy peacock feathers'
+        expected_list_text = '1: ' + new_todo_text
         # she types "Buy peacock feathers" into a tests box <insert humour>
-        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(new_todo_text)
 
         # when she hits enter the page updates, and now the page lists 
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
 
+        # Debugging:
+        #import time
+        #time.sleep(5)
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        
+        self.assertIn(expected_list_text, [row.text for row in rows])
 
         # there is still a text box inviting her to add another item.
         # she enters "Use peackock feathers to make a fly"
