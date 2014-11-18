fe249c979f8caecf9bc0d31e38d9c904fe08c4fc
peter.mulholland <peter.mulholland@wonga.com>
2014-11-18 18:04:08 +0000

    Python TDD saving the POST to the db

--- Python/TDD/superlists/lists/tests.py
+++ Python/TDD/superlists/lists/tests.py
@@ -31,6 +31,10 @@ class HomePageTest(TestCase):
 
         response = home_page(request)
 
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
         self.assertIn('A new list item', response.content.decode())
         expected_html = render_to_string(
                             'home.html', 
@@ -38,6 +42,11 @@ class HomePageTest(TestCase):
         )
 
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class ItemModelTest(TestCase):
