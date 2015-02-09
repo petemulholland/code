1ead2c57cde21f8ba96fcb31c84e58839a98d268
Peter Mulholland <peter.mulholland@yahoo.co.uk>
2015-02-09 21:59:50 +0000

    MagPi python pit issue 3 surfaces

--- magpi/python/issue03/pp0302ThreeSurfacesV3.py
+++ magpi/python/issue03/pp0302ThreeSurfacesV3.py
@@ -1,21 +1,23 @@
 # THREE SURFACES V2
 # By Jaseman - 13th June 2012
-
 import os, pygame; 
 from pygame.locals import *
 
 pygame.init(); 
 clock = pygame.time.Clock()
+
 os.environ['SDL_VIDEO_WINDOW_POS'] = 'center'
 
 pygame.display.set_caption("Three Surfaces")
-
 screen = pygame.display.set_mode([400,200],0,32) # The main screen
+
 sky = pygame.Surface((400,200)) # A sky surface
 sky.fill((200,255,255)) # Fill the surface in light blue color
+
 grass = pygame.Surface((400,100)) # A grass surface
 grass.fill((50,150,50)) # Fill the surface in green color
-sun = pyagme.Surface((80,80)) # A sun surface
+
+sun = pygame.Surface((80,80)) # A sun surface
 sun.set_colorkey([0,0,0])
 sun.set_alpha(128)
 pygame.draw.circle(sun,(255,255,0),(40,40),40)
