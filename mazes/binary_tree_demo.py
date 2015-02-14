d31d4a70e956062f9cc54e3143598288abdf09b9
Pete <peter.mulholland@yahoo.co.uk>
2015-02-14 19:43:59 +0000

    Mazes - tweak binary tree demo

--- mazes/binary_tree_demo.py
+++ mazes/binary_tree_demo.py
@@ -2,8 +2,8 @@ from mazes.grid import Grid
 from mazes.binary_tree import BinaryTree
 
 if __name__ == "__main__":
-	grid = Grid(8, 8)
-	print(grid)
+	grid = Grid(16, 16)
+	#print(grid)
 	bt = BinaryTree()
 	grid = bt.on(grid)
 	print(grid)
