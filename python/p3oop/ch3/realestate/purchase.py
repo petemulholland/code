e7a9bd77f8e743b0cd197e5f27eeac909bbf24f5
peter.mulholland <peter.mulholland@wonga.com>
2015-02-17 10:38:48 +0000

    P3 OOP - udpated chapter 3

--- python/p3oop/ch3/realestate/purchase.py
+++ python/p3oop/ch3/realestate/purchase.py
@@ -1,3 +1,18 @@
 class Purchase:
-	def __init__(self):
-		pass
+	def __init__(self, price='', taxes='', **kwargs):
+		super().__init__(**kwargs)
+		self.price = price
+		self.taxes = taxes
+		
+	def display(self):
+		super().display()
+		print("PURCHASE DETAILS")
+		print("selling price: {}".format(self.price))
+		print("estimated taxes: {}".format(self.taxes))
+		
+	def prompt_init():
+		return dict(
+			price=input("What is the selling price? "),
+			taxes=input("What are the estimated taxes? "))
+	prompt_init = staticmethod(prompt_init)
+	
--- python/p3oop/ch3/realestate/rental.py
+++ python/p3oop/ch3/realestate/rental.py
@@ -1,3 +1,21 @@
 class Rental:
-	def __init__(self):
-		pass
+	def __init__(self, furnished='', utilities='', rent='', **kwargs):
+		super().__init__(**kwargs)
+		self.furnished = furnished
+		self.rent = rent
+		self.utilities = utilities
+		
+	def display(self):
+		super().display()
+		print("PURCHASE DETAILS")
+		print("rent: {}".format(self.rent))
+		print("estimated utilities: {}".format(self.utilities))
+		print("furnished: {}".format(self.furnished))
+		
+	def prompt_init():
+		return dict(
+			rent=input("What is the monthly rent? "),
+			utilities=input("What are the estimated utilities? "),
+			furnished = get_valid_input("Is the property furnished? ", ("yes", "no")))
+	prompt_init = staticmethod(prompt_init)
+
