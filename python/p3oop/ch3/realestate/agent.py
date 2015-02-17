089e080ba099ee52ab6c4d18503df140439c592c
peter.mulholland <peter.mulholland@wonga.com>
2015-02-17 11:49:31 +0000

    P3 OOP ch3 - added agent functionality

--- python/p3oop/ch3/realestate/agent.py
+++ python/p3oop/ch3/realestate/agent.py
@@ -1,12 +1,32 @@
+from realestate.house_rental import HouseRental
+from realestate.house_purchase import HousePurchase
+from realestate.apartment_rental import ApartmentRental
+from realestate.apartment_purchase import ApartmentPurchase
+from realestate import get_valid_input
+
 class Agent():
-	property_list = []
-	
-	def __init__(self):
-		pass
+	type_map = {
+		("house", "rental"): HouseRental,
+		("house", "purchase"): HousePurchase,
+		("apartment", "rental"): ApartmentRental,
+		("apartment", "purchase"): ApartmentPurchase
+		}
 		
-	def list_properties(self, show_all = False):
-		pass
+	def __init__(self):
+		self.property_list = []
+		
+	def display_properties(self):
+		for property in self.property_list:
+			property.display()
 	
-	def add_property(self, property_type, purchase_type):
-		pass
+	def add_property(self):
+		property_type = get_valid_input(
+				"What type of property? ", 
+				("house", "apartment")).lower()
+		payment_type = get_valid_input(
+				"What payment type? ", 
+				("purchase", "rental")).lower()
+				
+		PropertyClass = self.type_map[(property_type, payment_type)]
+		self.property_list.append(PropertyClass(**PropertyClass.prompt_init()))
 		
