cffe1161ec4c01372e00295249a8f29165a3bcb6
peter.mulholland <peter.mulholland@wonga.com>
2015-03-24 09:51:08 +0000

    adding mcpi api mock to test code without mcpi connection

---
+++ minecraft/mcpi/minecraft.py
@@ -0,0 +1,35 @@
+from mcpi.vec3 import Vec3
+
+class Player:
+	def getPos(self):
+		return Vec3(0,0,0)
+		
+	def getTilePos(self):
+		return Vec3(0,0,0)
+		
+	def setPos(self, x, y, z):
+		pass
+		
+	def setTilePos(self, x, y, z):
+		pass
+
+class Minecraft:
+	def __init__(self):
+		self.player = Player
+	
+	@classmethod
+	def create(cls):
+		return Minecraft
+	
+	#create = classmethod(create)
+		
+	def setBlocks(self, x0,y0,z0, x1,y1,z1, id, data=0):
+		pass
+	
+	def setBlock(self, x,y,z, id, data=0):
+		pass
+	
+	def postToChat(self, message):
+		print message
+	
+	
---
+++ minecraft/mcpi/vec3.py
@@ -0,0 +1,9 @@
+class Vec3:
+	def __init(self, x, y, z):
+		self.x = x
+		self.y = y
+		self.y = y
+	
+	# TODO: copy real implementation here
+	
+		
