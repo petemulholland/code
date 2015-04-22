be42fc15607dbe283401bab63feb51fde86d9fd8
peter.mulholland <peter.mulholland@wonga.com>
2015-04-22 18:07:57 +0100

    minecraft started castle dining hall

--- minecraft/base/rooms/dining_hall.py
+++ minecraft/base/rooms/dining_hall.py
@@ -4,6 +4,12 @@ import mcpi.block as block
 from mcpi.block import Block
 from mcpi.vec3 import Vec3
 
+# http://minecraft.gamepedia.com/Data_values#Block_IDs
+from base.types import PlankData
+
+FLOORING = block.WOOD_PLANKS.withData(PlankData.DARK_OAK)
+PANELING = block.WOOD_PLANKS.withData(PlankData.ACACIA)
+
 class DiningHall(Building):
 	# TODO: implement table & chairs, fireplaces & paintings on walls
      #- dining hall 
@@ -13,6 +19,15 @@ class DiningHall(Building):
      #     - carpet on table at chair positions for place settings
      #     - => 13 x 9
      #     - maybe 15 to 17  long for fireplaces at  both ends  - 20 long?
-
-	pass
+	WALLS_CORNER_POS = {'South East' : Building.SE_CORNER_POS, 
+						'South West' : Building.SE_CORNER_POS + Vec3(-16,0,0),
+						'North West' : Building.SE_CORNER_POS + Vec3(-16,0,-10),
+						'North East' : Building.SE_CORNER_POS + Vec3(0,0,-10) }
+	
+	WIDTH = WALLS_CORNER_POS['South East'].x - WALLS_CORNER_POS['South West'].x
+	
+	# TODO: want to update/extend Building class to use ordered list of collections idea
+	# add blocks/block spans to a collection 
+	def __init__(self, *args, **kwargs):
+		super(DiningHall, self).__init__(width=DiningHall.WIDTH, *args, **kwargs)
 
