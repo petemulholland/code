5d9eae487f0bfb5575d2487b43e0015965829b7d
peter.mulholland <peter.mulholland@wonga.com>
2015-04-23 14:05:03 +0100

    minecraft missed target_practice in package housekeeping

--- minecraft/base/rooms/target_practice.py
+++ minecraft/base/rooms/target_practice.py
@@ -1,4 +1,4 @@
-from village.building import Building, BuildingLayer, BuildingBlock, CompositeBuilding, Torch
+from building import Building, BuildingLayer, BuildingBlock, CompositeBuilding, Torch
 import mcpi.block as block
 from mcpi.block import Block
 from mcpi.vec3 import Vec3
