edcd372dec19a0467c206287daa3c5c0c32bc16d
peter.mulholland <peter.mulholland@wonga.com>
2015-04-23 17:30:57 +0100

    minecraft docs on wood

--- minecraft/building/oriented_blocks.py
+++ minecraft/building/oriented_blocks.py
@@ -18,6 +18,7 @@ import random
 #	- dispenser
 #	- fence gate?
 #	- bookshelf
+#	- wood
 
 # NOTE: oriented block data appears to always indicate the direction the item is "facing"
 class OrientedBlock(BuildingBlock):
@@ -231,3 +232,33 @@ class Furnace(CommonOriented):
 		super(Furnace, self).__init__(block.FURNACE_INACTIVE, *args, **kwargs)
 
 
+#Wood NOTE: Acacia & Dark oak were added in 1.7
+#Block 17
+#	DV 	Description
+#	0 	Oak wood facing up/down
+#	1 	Spruce wood facing up/down
+#	2 	Birch wood facing up/down
+#	3 	Jungle wood facing up/down
+#	4 	Oak wood facing East/West
+#	5 	Spruce wood facing East/West
+#	6 	Birch wood facing East/West
+#	7 	Jungle wood facing East/West
+#	8 	Oak wood facing North/South
+#	9 	Spruce wood facing North/South
+#	10 	Birch wood facing North/South
+#	11 	Jungle wood facing North/South
+#	12 	Oak wood with only bark
+#	13 	Spruce wood with only bark
+#	14 	Birch wood with only bark
+#	15 	Jungle wood with only bark
+
+#Block 162
+#	DV 	Description
+#	0 	Acacia wood facing up/down
+#	1 	Dark Oak wood facing up/down
+#	4 	Acacia wood facing East/West
+#	5 	Dark Oak wood facing East/West
+#	8 	Acacia wood facing North/South
+#	9 	Dark Oak wood facing North/South
+#	12 	Acacia wood with only bark
+#	13 	Dark Oak wood with only bark
