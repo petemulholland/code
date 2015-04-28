c502b1e6d177c87cdacd655481275865064f44bb
peter.mulholland <peter.mulholland@wonga.com>
2015-04-28 16:55:40 +0100

    minecraft castle enclosure expansion

--- minecraft/base/castle_enclosure.py
+++ minecraft/base/castle_enclosure.py
@@ -1,86 +1,91 @@
 # castle enclosure plan
+# walls run from 6 - 74 == 7 - 75 => 68 long (N-S) /4 = 17 protrusion every 17 bocks would give 5 along each side including ends
+# 68 /3 = 22.6  - so 1 protrusion at 23 blocks from each corner in each direction
+# E-W walls are 6 - 73, so 67 long add protrusions 
+# walls should be 6 high & 2 wide & 4 deep below ground, moat is 3 deep & 3 wide filled with water
+
 #    8         7         6         5         4         3         2         1             
 #    098765432109876543210987654321098765432109876543210987654321098765432109876543210   
 #
 # 80                                                                                   08 
 #  9                                                                                   9 
-#  8                                                                                   8 
-#  7                                                                                   7 
-#  6                                                                                   6 
-#  5                                                                                   5 
-#  4                                                                                   4 
-#  3                                                                                   3 
-#  2                                                                                   2 
-#  1                                                                                   1 
-# 70                                                                                   07 
-#  9                                                                                   9 
-#  8                                                                                   8 
-#  7                                                                                   7 
-#  6                                                                                   6 
-#  5                                                                                   5 
-#  4                                                                                   4 
-#  3                                                                                   3 
-#  2                                                                                   2 
-#  1                                                                                   1 
-# 60                                                                                   06 
-#  9                                                                                   9 
-#  8                                                                                   8 
-#  7                           sss                        sss                          7 
-#  6                          s   s                      s   s                         6 
-#  5                         s     s                    s     s                        5 
-#  4                         s  s  ssssgsssgsssgsssgsssss  s  s                        4 
-#  3                         s                                s                        3 
-#  2                          s                              s                         2 
-#  1                           ss                          ss                          1 
-# 50                            s                          s                           05 
-#  9                            s                          s                           9 
-#  8                            s                          s                           8 
-#  7                            s                          s                           7 
-#  6                            s                          s                           6 
-#  5                            s                          s                           5 
-#  4                            s                          s                           4 
-#  3                            s                          s                           3 
-#  2                            s                          s                           2 
-#  1                            s                          s                           1 
-# 40                            d                          d                           04 
-#  9                            s                          s                           9 
-#  8                            s                          s                           8 
-#  7                            s                          s                           7 
-#  6                            s                          s                           6 
-#  5                            s                          s                           5 
-#  4                            s                          s                           4 
-#  3                            s                          s                           3 
-#  2                            s                          s                           2 
-#  1                            s                          s                           1 
-# 30                            s                          s                           03 
-#  9                            s                          s                           9 
-#  8                           ss                          ss                          8 
-#  7                          s                              s                         7 
-#  6                         s              sdds              s                        6 
-#  5                         s  s  ssssssssss  ssssssssss  s  s                        5 
-#  4                         s     s                    s     s                        4 
-#  3                          s   s                      s   s                         3 
-#  2                           sss                        sss X                        2 
-#  1                                                                                   1 
-#  0                                                                                   0 
-# 20                                                                                   02 
-#  9                                                                                   9 
-#  8                                                                                   8 
-#  7                                                                                   7 
-#  6                                                                                   6 
-#  5                                                                                   5 
-#  4                                                                                   4 
-#  3                                                                                   3 
-#  2                                                                                   2 
-#  1                                                                                   1 
-# 10                                                                                   01 
-#  9                                                                                   9 
-#  8                                                                                   8 
-#  7                                                                                   7 
-#  6                                                                                   6 
-#  5                                                                                   5 
-#  4                                                                                   4 
-#  3                                                                                   3 
+#  8      mmmmm                                                                        8 
+#  7     m     mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm    7 
+#  6    m  www                                                                    m    6 
+#  5   m  wwwww                                                                   m    5 
+#  4   m  wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww  m    4 
+#  3   m  wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww  m    3 
+#  2    m  www                                                     f          ww  m    2 
+#  1     m  ww                                                     f          ww  m    1 
+# 70     m  ww                                                     f          ww  m    07 
+#  9     m  ww                                                     f          ww  m    9 
+#  8     m  ww                                                     f          ww  m    8 
+#  7     m  ww                                                     f          ww  m    7 
+#  6     m  ww                                                     f          ww  m    6 
+#  5     m  ww                                                     f          ww  m    5 
+#  4     m  ww                                                     f          ww  m    4 
+#  3     m  ww                                                     f          ww  m    3 
+#  2     m  ww                                                     fffffffffffww  m    2 
+#  1     m  ww                                                     f          ww  m    1 
+# 60     m  ww                                                     f          ww  m    06 
+#  9     m  ww                                                     f          ww  m    9 
+#  8     m  ww                                                     f          ww  m    8 
+#  7     m  ww                 sss                        sss      f          ww  m    7 
+#  6     m  ww                s   s                      s   s     f          ww  m    6 
+#  5     m  ww               s     s                    s     s    f          ww  m    5 
+#  4     m  ww               s  s  ssssgsssgsssgsssgsssss  s  s    f          ww  m    4 
+#  3     m  ww               s                                s    f          ww  m    3 
+#  2     m  ww                s                              s     f          ww  m    2 
+#  1     m  ww                 ss                          ss      fffffffffffww  m    1 
+# 50     m  ww                  s                          s       f          ww  m    05 
+#  9     m  ww                  s                          s       f          ww  m    9 
+#  8     m  ww                  s                          s       f          ww  m    8 
+#  7     m  ww                  s                          s       f          ww  m    7 
+#  6     m  wws                 s                          s       f          ww  m    6 
+#  5     m  wws                 s                          s       f          ww  m    5 
+#  4     m  wws                 s                          s       f          ww  m    4 
+#  3     m  wws                 s                          s       f          ww  m    3 
+#  2     m  wws                 s                          s       f          ww  m    2 
+#  1     m  wws                 s                          s       f          ww  m    1 
+# 40     m  ww                  d                          d       fffffffffffww  m    04 
+#  9     m  wws                 s                          s       f          ww  m    9 
+#  8     m  wws                 s                          s       f          ww  m    8 
+#  7     m  wws                 s                          s       f          ww  m    7 
+#  6     m  wws                 s                          s       f          ww  m    6 
+#  5     m  wws                 s                          s       f          ww  m    5 
+#  4     m  wws                 s                          s       f          ww  m    4 
+#  3     m  ww                  s                          s       f          ww  m    3 
+#  2     m  ww                  s                          s       f          ww  m    2 
+#  1     m  ww                  s                          s       f          ww  m    1 
+# 30     m  ww                  s                          s       f          ww  m    03 
+#  9     m  ww                  s                          s       fffffffffffww  m    9 
+#  8     m  ww                 ss                          ss                 ww  m    8 
+#  7     m  ww                s                              s                ww  m    7 
+#  6     m  ww               s              sdds              s               ww  m    6 
+#  5     m  ww               s  s  ssssssssss  ssssssssss  s  s               ww  m    5 
+#  4     m  ww               s     s                    s     s               ww  m    4 
+#  3     m  ww                s   s                      s   s                ww  m    3 
+#  2     m  ww                 sss                        sss X               ww  m    2 
+#  1     m  ww                                                                ww  m    1 
+#  0     m  ww                                                                ww  m    0 
+# 20     m  ww                                                                ww  m    02 
+#  9     m  ww                                                                ww  m    9 
+#  8     m  ww                                                                ww  m    8 
+#  7     m  ww                                                                ww  m    7 
+#  6     m  ww                                                                ww  m    6 
+#  5     m  ww                                                                ww  m    5 
+#  4     m  ww                                                                ww  m    4 
+#  3     m  ww                                                                ww  m    3 
+#  2     m  ww                                                                ww  m    2 
+#  1     m  ww                                                                ww  m    1 
+# 10     m  ww                                                                ww  m    01 
+#  9     m  ww                                                                ww  m    9 
+#  8     m  ww                                                                ww  m    8 
+#  7     m  wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww  m    7 
+#  6     m  wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww  m    6 
+#  5     m                                                                        m    5 
+#  4     m                                                                        m    4 
+#  3     mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm    3 
 #  2                                                                                   2 
 #  1                                                                                   1 
 #  0                                                                                   0 
