c1723b4f3cd297056d317d51c5b4ea48307f57e9
Peter Mulholland <peter.mulholland@yahoo.co.uk>
2016-02-24 22:24:32 +0000

    updated git aliases

--- git_config.sh
+++ git_config.sh
@@ -5,7 +5,12 @@ git config --global user.email peter.mulholland@yahoo.co.uk
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
+git config --global alias.cim "commit -m"
 git config --global alias.st status
+git config --global alias.sh stash
+git config --global alias.shp "stash pop"
+git config --global alias.shl "stash list"
+git config --global alias.df diff
 git config --global alias.unstage "reset HEAD --"
 git config --global alias.last "log -1 HEAD"
 git config --global alias.hist "log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short"
